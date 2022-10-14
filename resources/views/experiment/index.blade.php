fdb82ba53ec4356241a02ee4aac6833553bd27d3
mluqmanhakim <mhd.luqmanhakim@gmail.com>
2022-10-14 16:04:00 +0200

    Initial commit

---
+++ resources/views/experiment/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.base')
+
+@section('extra_style')
+<link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+@endsection
+
+@section('content')
+
+
+<button id="button1" type="button">Click Me!</button>
+
+
+@endsection
+
+@section('extra_script')
+@endsection
